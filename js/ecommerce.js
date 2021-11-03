ad5caa17ff9cb5646d490dfaf312c27c899de32e
pablolopezar <pabloe.lopezarmenta@outlook.com>
2021-11-03 01:18:44 -0700

    Se modifica redireccionamiento

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
     $("#login").click(function () {
-        window.location.href="http://localhost:8080/ecommerce-uth-pwa/login.html";
+        window.location.href="http://192.168.1.10:8080/ecommerce-uth-pwa/login.html";
     })
 });
